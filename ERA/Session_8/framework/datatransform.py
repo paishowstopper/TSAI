7ae303c9866928b5760cff8defd0c96a826ea1e7
Mahesh R <paishowstopper@yahoo.co.in>
2023-07-14 23:21:14 +0530

    ERA Session 8, 9 and 10 assignments

--- ERA/Session_8/framework/datatransform.py
+++
@@ -1,8 +0,0 @@
-import torchvision.transforms as tf
-
-class DataTransformation:
-    def __init__(self):
-        pass
-
-    def CIFAR10Transform(self):
-        return tf.Compose([tf.ToTensor(), tf.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
